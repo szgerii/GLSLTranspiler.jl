cd83aca29e31ac8868a4f46a4399e10e9ea79505
szgeri <szucsi1104@gmail.com>
2025-07-31 13:18:13 +0200

    Update pkg version number

--- src/typed_ast/includes.jl
+++
@@ -1,9 +0,0 @@
-include("types/ASTNodes.jl")
-include("types/TASTNodes.jl")
-include("TypeTree.jl")
-include("VarData.jl")
-
-include("scopes.jl")
-include("rules.jl")
-include("expr_transforms.jl")
-include("build_tast.jl")
